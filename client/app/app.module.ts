0c9c97cd19cdaad6a65f253bb5dcc4ad45c37ed5
Mathis Møller <thisen-dk@hotmail.com>
2016-12-16 17:09:03 +0100

    Added webpack. Most taken from preboot/angular2-webpack. No testing added yet.

--- client/app/app.module.ts
+++ client/app/app.module.ts
@@ -1,11 +1,33 @@
-import { NgModule }      from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
+import {NgModule, ApplicationRef} from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
 
-import { AppComponent }  from './app.component';
+import {AppComponent}  from './app.component';
+import {createNewHosts, removeNgStyles} from "@angularclass/hmr";
 
 @NgModule({
-  imports:      [ BrowserModule ],
-  declarations: [ AppComponent ],
-  bootstrap:    [ AppComponent ]
+  imports: [BrowserModule],
+  declarations: [AppComponent],
+  bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+  constructor(public appRef: ApplicationRef) {
+  }
+
+  hmrOnInit(store) {
+    console.log('HMR store', store);
+  }
+
+  hmrOnDestroy(store) {
+    let cmpLocation = this.appRef.components.map(cmp => cmp.location.nativeElement);
+    // recreate elements
+    store.disposeOldHosts = createNewHosts(cmpLocation);
+    // remove styles
+    removeNgStyles();
+  }
+
+  hmrAfterDestroy(store) {
+    // display new elements
+    store.disposeOldHosts();
+    delete store.disposeOldHosts;
+  }
+}
